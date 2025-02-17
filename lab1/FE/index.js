70fed8027b8ba6776dbc5160fb066d6ebe0b9e6f
Menna Elawadly <133363653+Mennasalah140@users.noreply.github.com>
2025-02-17 22:32:30 +0200

    FE Done

--- lab1/FE/index.js
+++ lab1/FE/index.js
@@ -31,22 +31,55 @@ function fetchEmployees() {
 
 // TODO
 // add event listener to submit button
+const submitButton = document.querySelector('button[type="submit"].btn-primary');
+submitButton.addEventListener("click", createEmployee);
 
 // TODO
 // add event listener to delete button
+document.getElementById('dataTable').addEventListener("click", function(event){
+  if (event.target.classList.contains('btn-danger')){
+    deleteEmployee(event)
+  }
+});
 
 // TODO
 function createEmployee (){
   // get data from input field
+  const id = document.getElementById("id").value;
+  const name = document.getElementById("name").value;
+  const data = {id,name};
+
   // send data to BE
-  // call fetchEmployees
+  fetch(`http://localhost:3000/api/v1/employee`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json', 
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => response.json())
+    .then(data => {
+    // call fetchEmployees
+      fetchEmployees(); 
+    })
+    .catch(error => console.error(error))
 }
 
 // TODO
-function deleteEmployee (){
+function deleteEmployee (event){
   // get id
+  const id = event.target.parentElement.parentElement.cells[0].textContent;
+
   // send id to BE
-  // call fetchEmployees
+  fetch(`http://localhost:3000/api/v1/employee/${id}`, {
+    method: 'DELETE',
+  })
+    .then(response => response.json())
+    .then(data => {
+    // call fetchEmployees
+      fetchEmployees(); 
+    })
+    .catch(error => console.error(error))
 }
 
 fetchEmployees()
