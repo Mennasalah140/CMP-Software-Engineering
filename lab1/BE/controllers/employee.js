2d8559eec9f25bbdc5ee909473f6c2b76531d0e7
M7md03 <tifa1862003@gmail.com>
2025-02-18 23:59:24 +0200

    BE done

--- lab1/BE/controllers/employee.js
+++ lab1/BE/controllers/employee.js
@@ -8,8 +8,33 @@ exports.getEmployees = async (req, res, next) => {
 
 // TODO
 exports.deleteEmployee = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const index = employee.findIndex(emp => emp.id === id);
+    if (employee.some(emp => emp.id === id)) {
+      employee.splice(index, 1);
+      res.status(200).json({ message: 'Employee deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'Employee not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
 // TODO
 exports.createEmployee = async (req, res, next) => {
+  try {
+    const { id, name } = req.body;
+    if (id && name && !employee.some(emp => emp.id === id)) {
+      employee.push({ id, name });
+      res.status(201).json({ message: 'Employee created successfully' });
+    } else {
+      res.status(400).json({ message: 'Invalid employee data' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
